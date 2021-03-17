75e587b4efad3a873134de9d3446f02f095b6a5f
Vincenzo <vincenzorm117@gmail.com>
2021-03-16 23:09:22 -0400

    Added route53 A record to cloudfront distribution

--- dns.tf
+++ dns.tf
@@ -7,7 +7,7 @@ resource "aws_route53_record" "cert_validation" {
   # records = [aws_acm_certificate.cert.domain_validation_options.0.resource_record_value]
   # ttl     = 60
 
-   for_each = {
+  for_each = {
     for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
@@ -26,4 +26,16 @@ resource "aws_route53_record" "cert_validation" {
 data "aws_route53_zone" "main" {
   name         = var.hostname
   private_zone = false
+}
+
+resource "aws_route53_record" "mombo-cloudfront-route53" {
+  zone_id = data.aws_route53_zone.main.zone_id
+  name    = var.hostname
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.mombo-deploy[0].domain_name
+    zone_id                = aws_cloudfront_distribution.mombo-deploy[0].hosted_zone_id
+    evaluate_target_health = false
+  }
 }
