628ede3df7e80f3e62ccc015ff4e34d8187b2ea4
Vincenzo <vincenzorm117@gmail.com>
2021-07-24 19:16:10 -0400

    Updated acm dns settings to use loops based on input variables

--- acm.tf
+++ acm.tf
@@ -1,11 +1,8 @@
 
 resource "aws_acm_certificate" "cert" {
-  domain_name       = var.hostname
-  validation_method = "DNS"
+  for_each                  = local.domains
+  domain_name               = each.value
+  validation_method         = "DNS"
+  subject_alternative_names = ["*.${each.value}"]
 }
 
-resource "aws_acm_certificate_validation" "cert" {
-  certificate_arn = aws_acm_certificate.cert.arn
-  # validation_record_fqdns = [aws_route53_record.cert_validation.fqdn]
-  validation_record_fqdns = [for record in aws_route53_record.cert_validation : record.fqdn]
-}
--- dns.tf
+++ dns.tf
@@ -1,41 +1,21 @@
 
 
-resource "aws_route53_record" "cert_validation" {
-  # name    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_name
-  # type    = aws_acm_certificate.cert.domain_validation_options.0.resource_record_type
-  # zone_id = data.aws_route53_zone.main.id
-  # records = [aws_acm_certificate.cert.domain_validation_options.0.resource_record_value]
-  # ttl     = 60
-
-  for_each = {
-    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
-
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = data.aws_route53_zone.main.zone_id
-}
-
 data "aws_route53_zone" "main" {
-  name         = var.hostname
+  for_each     = local.domains
+  name         = each.value
   private_zone = false
 }
 
-resource "aws_route53_record" "mombo-cloudfront-route53" {
-  zone_id = data.aws_route53_zone.main.zone_id
-  name    = var.hostname
-  type    = "A"
 
-  alias {
-    name                   = aws_cloudfront_distribution.mombo-deploy.domain_name
-    zone_id                = aws_cloudfront_distribution.mombo-deploy.hosted_zone_id
-    evaluate_target_health = false
-  }
-}
+
+# resource "aws_route53_record" "mombo-cloudfront-route53" {
+#   zone_id = data.aws_route53_zone.main.zone_id
+#   name    = var.hostname
+#   type    = "A"
+
+#   alias {
+#     name                   = aws_cloudfront_distribution.mombo-deploy.domain_name
+#     zone_id                = aws_cloudfront_distribution.mombo-deploy.hosted_zone_id
+#     evaluate_target_health = false
+#   }
+# }
