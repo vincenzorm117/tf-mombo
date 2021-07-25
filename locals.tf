b831b89d68e45c730a1022e2604259c53e36138d
Vincenzo <vincenzorm117@gmail.com>
2021-07-24 22:32:11 -0400

    Setup locals

---
+++ locals.tf
@@ -0,0 +1,19 @@
+locals {
+  domains = toset([for domain in setunion(
+    toset([for s in var.static_sites : s.hostname]),
+    toset([for s in var.apis : s.hostname]),
+  ) : regex("[^.]+.[^.]+$", domain)])
+
+  # Maps all full domain sites like: 
+  #   api.example.com
+  #   my.www.example.com
+  #  To:
+  #   example.com
+  static_site_domains_to_root_domain = {
+    for site in var.static_sites : site.hostname => regex("[^.]+.[^.]+$", site.hostname)
+  }
+
+  static_sites = {
+    for site in var.static_sites : site.hostname => site
+  }
+}
